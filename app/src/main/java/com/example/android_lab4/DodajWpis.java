3d130c8199a19377eca56ac17f672629f5fe0dc6
akrepski <ilpiqs@gmail.com>
2021-05-29 12:38:08 +0200

    final

--- app/src/main/java/com/example/android_lab4/DodajWpis.java
+++ app/src/main/java/com/example/android_lab4/DodajWpis.java
@@ -2,7 +2,11 @@ package com.example.android_lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DodajWpis extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class DodajWpis extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_wpis);
+    }
+
+    public void wyslij(View view)
+    {
+        EditText kontrolka = (EditText) findViewById(R.id.editText);
+        String pole = kontrolka.getText().toString();
+        Intent intent = new Intent();
+        intent.putExtra("wpis", pole);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/android_lab4/MainActivity.java
+++ app/src/main/java/com/example/android_lab4/MainActivity.java
@@ -41,6 +41,17 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, 1);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            String nowy = (String) extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
     private ArrayList<String> target;
     private ArrayAdapter adapter;
 }
